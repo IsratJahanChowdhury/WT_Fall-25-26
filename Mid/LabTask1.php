da05c986a6553e5fa3899dfeb93e6ad63385bc54
HP <eshachwdhry@gmail.com>
2025-11-03 14:46:31 +0600

    First commit

---
+++ Mid/LabTask1.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>wt_O</title>
+</head>
+<body>
+   
+<style>
+     
+    body{
+        background-color: rgb(242,219,231);
+    }
+    .para{
+         width: 500px;
+        margin: auto;
+        background-color: white;
+        padding: 20px;
+    }
+     h2
+     {
+       text-decoration-line:underline;
+       text-decoration-color:red;
+       color:red;font-size:18px
+
+     }
+    </style>
+
+    <div class="para">
+        <h1> Student Registration Form </h1>  
+        
+        
+
+<p>
+First name
+<br>
+<input type="text">
+
+<br>
+</br>
+Last name
+<br>
+<input type="text">
+<br>
+</br>
+
+Student ID
+<br>
+<input type="number">
+<br>
+</br>
+Program
+<br>
+<input type="dropbox"> Bsc in CSE
+
+<input type="dropbox"> Bsc in EEE
+
+
+Department
+<br>
+<input type="text">
+</br>
+<br>
+Phone
+<br>
+<input type="number">
+<br>
+</br>
+
+University mail
+<br>
+<input type="text">
+<br>
+</br>
+Create password
+<br>
+<input type="number">
+<br>
+</br>
+Confirm Password
+<br>
+<input type="number">
+<br>
+</br>
+
+Semester Selection
+<br>
+</br>
+<input type="radio" name ="s"> Summer 2024
+<input type="radio" name ="s"> Fall 2024
+<input type="radio" name ="s"> Spring 2025
+<input type="radio" name ="s"> Other/Special Term
+<br>
+
+</br>
+Requested Credit Load (Max 15)
+<br>
+<input type="number">
+</br>
+<br>
+<input type="checkbox"> I require academic advising before final registration
+</br>
+<h2>Course Selection</h2>
+
+<input type="checkbox"> MATH 1201
+<input type="checkbox"> CS 2105
+<input type="checkbox"> ECON 1001
+<input type="checkbox">  PHY 1102
+</br>
+
+Comments
+<br>
+<input type="text">
+
+<center>
+<br>
+<input type= "Submit">
+</br>
+</center>
+<center>
+<br>
+<input type= "Reset">
+</br>
+</center>
+</div>
+
+</p>
+</body>
+</html>
+
+
+
+
+
+
