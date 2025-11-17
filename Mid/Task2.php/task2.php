6956e8aadeea4309e7b34599d451f5bd3b8c5394
HP <eshachwdhry@gmail.com>
2025-11-17 17:32:16 +0600

    second

--- Mid/Task2.php/task2.php
+++ Mid/Task2.php/task2.php
@@ -8,5 +8,26 @@
       padding: 30px;
       background-color: #eef4ff;
     }
-    
+     h2 {
+     text-align: center;
+     color: rgba(3, 8, 14, 1);
+    }
+    form {
+     background-color: #ffffff;
+     padding: 20px;
+     border-radius: 10px;
+     width: 300px;
+     margin: 0 auto;
+     box-shadow: 0 0 10px rgba(0,0,0,0.1);
+    }
+    input, button { 
+        width: 100%;
+        padding: 8px;
+        margin-top: 10px;
+        border-radius: 5px;
+        border: 1px solid #ccc;
+    }
+
    
+
+    
