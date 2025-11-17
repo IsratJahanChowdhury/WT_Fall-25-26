3e600058d2892d27fa48a5c408ee4ef0d6d87532
HP <eshachwdhry@gmail.com>
2025-11-17 17:16:43 +0600

    formcommit

---
+++ Mid/Task2.php/task2.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Student Registration & Course List</title>
+<style>
+    body {
+      font-family: Arial, sans-serif;
+      padding: 30px;
+      background-color: #eef4ff;
+    }
