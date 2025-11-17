121c1bc9682a23605ab98a48d41c95269ca5d7ca
HP <eshachwdhry@gmail.com>
2025-11-17 12:39:54 +0600

    second commit

--- task2.php
+++ task2.php
@@ -8,4 +8,21 @@
                background-color: #eef7ff;
                padding: 20px;
            }
+</style>
+</head>
+<body>
+<h2> Student Registration</h2>
+<form onsubmit="return handleSubmit()">
+<label>Full Name:</label>
+<input type="text" id="fullname">
+<label>Email:</label>
+<input type="text" id="email">
+<label>Password:</label>
+<input type="password" id="password">
+<label>Confirm Password:</label>
+<input type="password" id="confirm">
+<button type="submit">Submit</button>
+</form>
+</body>
+
 </html>
