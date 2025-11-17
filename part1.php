0577c808026f1096e328c8a8384f7ec0107d751a
HP <eshachwdhry@gmail.com>
2025-11-17 13:04:03 +0600

    second commit

---
+++ part1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Student Registration & Course List</title>
+<style>
+           body{
+               font-family: Arial, sans-serif;
+               background-color: #eef7ff;
+               padding: 20px;
+           }
+</html>
